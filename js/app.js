9d626b75472fd9179678246a80374ade7c969822
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-04-28 19:58:05 +0200

    Initial JS content

--- js/app.js
+++ js/app.js
@@ -2,5 +2,38 @@ $(document).ready(function(){
 
 
 
-	
+	var question1 = "What is the capital of Kenya?"
+
+	var question2 = "What was the first country to win the Football World Cup in 1930?"
+
+	var question3 = "Something which is 'Hellenic' comes from which country?"
+
+	var question4 = "When was the Eiffel Tower built?"
+
+	var question5 = "What is the southernmost capital city in the world?"
+
+	var question6 = "The Pythagorian theorem is a geometrical relation concerning which shape?"
+
+
+
+	var quiz = [question1, question2, question3, question4]
+
+
+
+
 }
+
+//Answers:
+	//1 Timbuktu, Nairobi, Kinshasa
+	//2 Italy, Brasil, Uruguay
+	//3 Greece, Sweden, Iceland
+	//4 1908, 1889, 1934
+	//5 Canberra Australia, Santiago Chile, Wellington New Zealand
+	//6 Circle, acute triangle, right triangle
+
+
+
+	//to show progression in the game 'question 3 of 5'
+	//for (var i = 0; i < quiz.length; i++) {
+	//    console.log("Question " + i + " of " + quiz.length);
+	//}
